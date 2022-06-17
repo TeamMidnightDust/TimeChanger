527c625871a5df7f9fa598b89c2bc9dee2ddf9fb
Emma Cypress Pointer-Null <emmaffle@modrinth.com>
2022-06-17 17:35:39 -0400

    Update to 1.19

--- src/main/java/eu/midnightdust/timechanger/TimeChangerClient.java
+++ src/main/java/eu/midnightdust/timechanger/TimeChangerClient.java
@@ -5,7 +5,7 @@ import eu.midnightdust.timechanger.config.TimeChangerConfig;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.command.v1.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 
 public class TimeChangerClient implements ClientModInitializer {
 
@@ -15,7 +15,7 @@ public class TimeChangerClient implements ClientModInitializer {
     public void onInitializeClient() {
         AutoConfig.register(TimeChangerConfig.class, JanksonConfigSerializer::new);
         TC_CONFIG = AutoConfig.getConfigHolder(TimeChangerConfig.class).getConfig();
-        ClientCommandManager.DISPATCHER.register(ClientCommandManager.literal("ctime")
+        ClientCommandManager.getActiveDispatcher().register(ClientCommandManager.literal("ctime")
                 .then(CTimeCommand.command())
         );
     }
